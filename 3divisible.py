b464381fd4ead990083e92ca04e01829732ef693
sathvikavarma <55590704+sathvikavarma@users.noreply.github.com>
2021-03-11 19:29:21 +0530

    Add files via upload

---
+++ 3divisible.py
@@ -0,0 +1,2 @@
+list1 = [2, 4, 6, 8, 10, 12]
+print ([x for x in list1 if x & 3 == 0])
---
+++ expo.py
@@ -0,0 +1,5 @@
+x, y = [int(x) for x in input(" ").split()]
+result = 1 
+for i in range(y): 
+  result *= x 
+print(result)
